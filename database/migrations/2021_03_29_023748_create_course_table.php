55366ce7e7465299f8aa2b726aa5374221f2f336
somosappeducativa <somosappeducativa@gmail.com>
2021-03-28 22:15:40 -0500

    texto

---
+++ database/migrations/2021_03_29_023748_create_course_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('slug',100)->nullable();
+            $table->string('excerpt',100)->nullable();
+            $table->string('course',100)->nullable();
+            $table->text('description')->nullable();
+            $table->string('room',100)->nullable();
+            $table->string('level',100)->nullable();
+            $table->string('descriptionTask',100)->nullable();
+            $table->string('amountTask',100)->nullable();
+            $table->integer('calification')->nullable();
+            $table->text('json')->nullable();
+            $table->integer('subject',255)->nullable();
+            $table->string('parentId',100)->nullable();
+            $table->string('notification',100)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course');
+    }
+}
