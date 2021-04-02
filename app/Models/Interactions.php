f740bda936314ea2789531568bb99430d2c716af
Your Name <desarrollador2@alcanosesp.com>
2021-04-02 16:37:37 -0500

    cargue por consola de factories, models, controllers

--- app/Models/Interactions.php
+++ app/Models/Interactions.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Course extends Model
+class Interactions extends Model
 {
     use HasFactory;
 }
